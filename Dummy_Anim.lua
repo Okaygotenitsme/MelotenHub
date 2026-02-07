ce87656b00616466a2da300647b0be622c511699
Yakrutoyarbuzoreh <totorkulovredzeb@gmail.com>
2026-02-07 20:28:17 +0300

    Update Dummy_Anim.lua

--- Dummy_Anim.lua
+++ Dummy_Anim.lua
@@ -1,3 +1,3 @@
 script_key = "%KEY%"
 
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/c41caee69bedf5a8aee68e2582b14e8de06a5ccb8dd80748480577c611892976.lua"))()
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/ad0dd8a506b6a6350713dd2e8639de4988f99bc4d8405a94e3223bad93b2b244.lua"))()
